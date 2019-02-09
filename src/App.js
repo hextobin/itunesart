0d48c4661baf8bbd8d9d05dab0a3b08a2f954df6
Spencer Brooks <spencer@databit.io>
2019-02-08 18:03:57 -0700

    initial and add art card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetchJsonp from 'fetch-jsonp'
+import axios from 'axios'
+import 'semantic-ui-css/semantic.min.css'
+import ArtCard from './components/ArtCard'
 
 class App extends Component {
+
+  state = {
+    resp: null,
+    anyData: false,
+  }
+
+  componentDidMount() {
+    fetchJsonp('https://itunes.apple.com/search?term=jack+johnson&limit=25')
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        this.setState({ resp: json, anyData: true})
+      }).catch((ex) => {
+        console.log('parsing failed', ex)
+      })
+  }
+  
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ArtCard>
+
+        </ArtCard>
       </div>
     );
   }
---
+++ src/components/ArtCard.js
@@ -0,0 +1,16 @@
+import { Card } from 'semantic-ui-react'
+import React, { Component } from 'react'
+
+class ArtCard extends Component {
+  render(){
+    return(
+      <div>
+        <Card>
+          
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default ArtCard
